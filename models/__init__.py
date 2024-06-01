c92450c7d933f37adaeb9ff2431ac8789664dfa0
lmurayire <l.murayire@alustudent.com>
2024-06-01 03:53:10 -0700

    init file_ models

--- models/__init__.py
+++ models/__init__.py
@@ -10,10 +10,3 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from os import getenv
-
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-storage.reload()
